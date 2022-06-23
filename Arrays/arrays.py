0ac71e79ca2904f4c9d2403e90a806287523c18b
Renan <renan.lowe@hotmail.com>
2022-06-23 13:30:27 -0300

    creating array & making examples

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -1,1 +1,9 @@
-print('Hello')
+array = ['Renan', 'Camila', 'Duda', 'Joao']
+
+print(f'{array[0]}:')
+print(f'-Bom dia, {array[1]}')
+
+print('\n\n-> Elementos da lista:')
+i = 0
+for i in range(len(array)):
+    print(f'*{array[i]}')
