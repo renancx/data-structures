97e7792a346a60b49fd1d3ed20cc8fa73fb681f6
Renan <renan.lowe@hotmail.com>
2022-06-23 14:01:23 -0300

    create some methods examples

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -3,7 +3,17 @@ array = ['Renan', 'Camila', 'Duda', 'Joao']
 print(f'{array[0]}:')
 print(f'-Bom dia, {array[1]}')
 
-print('\n\n-> Elementos da lista:')
+print('\n-> Elementos da lista:')
 i = 0
 for i in range(len(array)):
     print(f'*{array[i]}')
+
+print('\n-> Array antiga:')
+print(array)
+
+array.append('Tabaldi') #coloca na ultima posicao
+array.remove('Duda') #remove da array
+array.insert(2,'Igor') #insere na posicao informada
+
+print('\n-> Array atual:')
+print(array)
