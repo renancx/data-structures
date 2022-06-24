4a3061f160d3bbff5613798c9aca2adc100ca558
Renan <renan.lowe@hotmail.com>
2022-06-24 13:20:30 -0300

    do some stacks examples

--- Stacks/stacks.py
+++ Stacks/stacks.py
@@ -1,1 +1,16 @@
-print('Hello')
+stack = []
+
+#em python, os metodos append e pop
+#adicionam e removem o ultimo elemento da matriz
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print('Stack inicial')
+print(stack)
+
+stack.pop() #remove o ultimo elemento da pilha
+stack.pop()
+
+print('\nStack final')
+print(stack)
