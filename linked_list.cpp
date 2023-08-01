3c0bb53e342d8a17c15d34072e72496a618ba0a9
Renan <renan.lowe@hotmail.com>
2023-08-01 14:12:14 -0300

    one element example

--- linked_list.cpp
+++ linked_list.cpp
@@ -6,8 +6,23 @@ struct node {
 };
 
 node *head; //global variable, can be accessed anywhere
+node *renan;
 
 int main(){
     head = NULL; //empty list
     std::cout << head << std::endl; //a lista está vazia, então o endereço é 0
+
+    renan = new node(); //cria um novo nó
+    renan->data = 99;
+    renan->next = NULL;
+    std::cout << renan << std::endl; //endereço do nó
+
+    head = renan; //head aponta para o nó renan
+    std::cout << head << std::endl; //endereço do nó renan
+
+    //head nao tem valor, mas aponta para o nó renan
+    //head nunca vai ter valor, mas vai apontar para o primeiro nó da lista
+
+    
+
 }
