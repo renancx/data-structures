b5d085d415d40c80d8160bf354392a415918e2a0
Renan <renan.lowe@hotmail.com>
2023-08-11 11:02:38 -0300

    edge list representation of a graph

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -2,6 +2,8 @@
 
 //edge list representation
 
+//edge list can be used to represent a graph in a very simple way but it is not very efficient
+
 std::string vertices[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
 int edges[][3] = {{0, 1, 3}, {0, 3, 2}, {1, 2, 6}, {1, 4, 7}, {2, 5, 7}, {3, 4, 2}, {3, 6, 1}, {4, 5, 8}, {4, 7, 3}, {5, 8, 1}, {6, 7, 2}, {7, 8, 1}};
 
